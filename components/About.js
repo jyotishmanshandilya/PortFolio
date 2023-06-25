a49caf359ad5b7becf008b4f4de85a211b47a030
Jyotishman Shandilya <jyotishmanshandilyaggn@gmail.com>
2023-06-25 17:28:45 +0530

    typo fixes and project addition

--- components/About.js
+++ components/About.js
@@ -12,10 +12,9 @@ const About = () => {
             Hi <b>{session?.user?.name}</b>, I am Jyotishman Shandilya, your not so average dog loving, gymrat buddy.
             I am a <b>fullstack web developer</b> whose work mostly revolves around designing websites for 
             clients and building some personal projects. I am also working on building my digital 
-            marketing agency called <b><a className='italic underline' href='https://dascii.com'>DASCII</a></b> where we provide businesses 
-            with brand enahncing services such, marketing, devlopment and design. Feel free to reach 
-            out to me regarding any work or if you just discuss some ideas. You can contact me by 
-            clicking on the button below
+            marketing and development agency called <b><a className='italic underline' href='https://dascii.com'>DASCII</a></b> where we provide businesses 
+            with brand enhancing services such, marketing, development and design. Feel free to reach 
+            out to me regarding any work or if you just discuss some ideas.
         </p>
         {/* <button className='outline_btn'>Contact me</button> */}
     </div>
--- components/projectdata.js
+++ components/projectdata.js
@@ -10,6 +10,14 @@ export default {
         },
         {
             'id': 2,
+            'title': 'PromptOVerse',
+            'description': 'PromptOverse is an open source AI prompt sharing and finding platform where users can find amazing prompts as well as share their creative prompts. Made using nextjs and MongoDB ',
+            'link': 'https://promptoverse.vercel.app/',
+            'img':'promptoverse.png',
+            'code':'https://github.com/jyotishmanshandilya/Promptoverse'
+        },
+        {
+            'id': 3,
             'title': 'Travel Journal',
             'description': 'Travel blog made using react framework of javascript. Data entered in the data javascript file automatically reflects in the form or a newly formed styled card',
             'link': 'https://master.dr6x82hw0rmx9.amplifyapp.com/',
@@ -17,7 +25,7 @@ export default {
             'code':'https://github.com/jyotishmanshandilya/TravelJournal-ReactApp/tree/master'
         },
         {
-            'id': 3,
+            'id': 4,
             'title': 'Meme Generator',
             'description': 'Random meme generation app made using react framework of javascript. Fetches memes from an api and display them along with the phrases entered by the user to make a meme',
             'link': 'https://master.d35e68r7elc3w3.amplifyapp.com/',
@@ -25,7 +33,7 @@ export default {
             'code': 'https://github.com/jyotishmanshandilya/MemeGenerator/tree/master'
         },
         {
-            'id': 4,
+            'id': 5,
             'title': 'Tic Tac Toe',
             'description': 'Tic tac toe game app made using react framework of javascript. Allows players to play in realtime and reset after each game to continue wihtout refreshing',
             'link': 'https://main.d2wa1g5go5txnn.amplifyapp.com/',
