234079b2097d689c010c1ba697f63d0e7945d1db
Jyotishman Shandilya <jyotishmanshandilyaggn@gmail.com>
2023-07-29 18:19:04 +0530

    added react tilt to techstack cards

--- components/Techstack.js
+++ components/Techstack.js
@@ -3,21 +3,18 @@ import React from 'react'
 import Techstackcard from './Techstackcard'
 import techdata from './techdata';
 
-const Techstack = () => {
-    // techdata.data.map((item)=>{
-    //     console.log(item.logo);
-    // })
 
-    
+const Techstack = () => {    
   return (
-    <div className='mx-auto mb-5'>
-        <h2 className='text-center'>TechStack</h2>
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 mx-8 my-5'>
-            {techdata.data.map((item) => (
-                <Techstackcard key={item.id} tech={item.tech} logo={item.logo}/>
-            ))}
-        </div>
-    </div>
+    
+      <div className='mx-auto mb-5'>
+          <h2 className='text-center'>TechStack</h2>
+          <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 mx-8 my-5'>
+              {techdata.data.map((item) => (
+                  <Techstackcard key={item.id} tech={item.tech} logo={item.logo}/>
+              ))}
+          </div>
+      </div>
   )
 }
 
--- components/Techstackcard.js
+++ components/Techstackcard.js
@@ -7,23 +7,38 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
+import { Tilt } from 'react-tilt'
+
+const defaultOptions = {
+	reverse:        false,  // reverse the tilt direction
+	max:            35,     // max tilt rotation (degrees)
+	perspective:    1000,   // Transform perspective, the lower the more extreme the tilt gets.
+	scale:          1.1,    // 2 = 200%, 1.5 = 150%, etc..
+	speed:          1000,   // Speed of the enter/exit transition
+	transition:     true,   // Set a transition on enter/exit.
+	axis:           null,   // What axis should be disabled. Can be X or Y.
+	reset:          true,    // If the tilt effect has to be reset on exit.
+	easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+}
 
 const Techstackcard = (props) => {
     const theme = useTheme();
     return (
-        <Card className='grid grid-cols-2'>
-          <Box className='align-middle'>
-            <Typography className='text-center pt-9' component="div">
-              {props.tech}
-            </Typography>
-          </Box>
-          <CardMedia
-            component="img"
-            sx={{ width: 200, height: 100 }}
-            image={props.logo}
-            alt={`${props.tech} logo`}
-          />
-        </Card>
+      <Tilt options={defaultOptions} style={{ height: 250, width: 250 }}>
+          <Card className='grid grid-cols-2'>
+            <Box className='align-middle'>
+              <Typography className='text-center pt-9' component="div">
+                {props.tech}
+              </Typography>
+            </Box>
+            <CardMedia
+              component="img"
+              sx={{ width: 200, height: 100 }}
+              image={props.logo}
+              alt={`${props.tech} logo`}
+            />
+          </Card>
+        </Tilt>
     );
 }
 
